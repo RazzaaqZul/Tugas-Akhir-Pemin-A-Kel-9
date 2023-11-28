9f132843c7ea673a93c5a396985dc39faf010279
RazzaaqZul <112380919+RazzaaqZul@users.noreply.github.com>
2023-11-28 21:28:44 +0700

    web.php (mengatur route sesuai modul 10)

--- modul_10_kel_9/routes/web.php
+++ modul_10_kel_9/routes/web.php
@@ -1,26 +1,32 @@
-
-
 <?php
 use Illuminate\Support\Str;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MataKuliahController;
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
+    //  Muhammad Razzaaq Zulkahfi - 215150700111047
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
 
-$router->get('/key', function () {
-    return Str::random(32);
-    });
+$router->group(['prefix' => 'auth'], function () use ($router) {
+    $router->post('/register', 'AuthController@register');
+    $router->post('/login', 'AuthController@login');
+});
+
+$router->get('/mahasiswa', 'MahasiswaController@getMahasiswa');
+    //  Muhammad Razzaaq Zulkahfi - 215150700111047
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->get('/mahasiswa/profile', 'MahasiswaController@getMahasiswaByToken');
+    $router->post('/mahasiswa/matakuliah/{mkId}', 'MataKuliahController@postMatkulMahasiswa');
+    $router->put('/mahasiswa/matakuliah/{mkId}', 'MataKuliahController@putMatkulMahasiswa');
+});
+    //  Muhammad Razzaaq Zulkahfi - 215150700111047
+$router->get('/mahasiswa/{nim}', 'MahasiswaController@getMahasiswaByNIM');
+
+$router->get('/prodi', 'ProdiController@getProdi');
+
+$router->get('/matakuliah', 'MataKuliahController@getMataKuliah');
